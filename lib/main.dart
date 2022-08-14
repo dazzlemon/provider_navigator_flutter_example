541735ba8987bf8b1c01acbd50da4e782d9bb51d
dazzlemon <daanill13470@gmail.com>
2022-08-15 02:22:22 +0300

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() =>
-  runApp(ChangeNotifierProvider(
-		create: (_) => ThemeModel(),
+  runApp(MultiProvider(
+		providers: [
+			ChangeNotifierProvider<ThemeModel>(create: (_) => ThemeModel(false)),
+			ChangeNotifierProvider<VolumeModel>(create: (_) => VolumeModel(15, 0, 100))
+		],
 		child: const MyApp()
 	));
 
 class ThemeModel extends ChangeNotifier {
-	var isDarkMode = false;
+	bool _isDarkMode;
+	ThemeModel(this._isDarkMode);
 
+	bool get isDarkMode => _isDarkMode;
 	ThemeMode get mode => isDarkMode ? ThemeMode.dark  : ThemeMode.light;
 	IconData  get icon => isDarkMode ? Icons.dark_mode : Icons.light_mode;
 
 	void toggle() {
-		isDarkMode = !isDarkMode;
+		_isDarkMode = !isDarkMode;
 		notifyListeners();
+	}
+}
+
+class VolumeModel extends ChangeNotifier {
+	double _value;
+	final double max;
+	final double min;
+	VolumeModel(this._value, this.min, this.max);
+
+	double get value => _value;
+
+	set value(double newValue) {
+		if (newValue >= min && newValue <= max && newValue != _value) {
+			_value = newValue;
+			notifyListeners();
+		}
 	}
 }
 
@@ -27,11 +48,28 @@ class MyApp extends StatelessWidget {
 		Consumer<ThemeModel>(
 			builder: (_, theme, __) =>
 				MaterialApp(
+					initialRoute: '/',
+					routes: {
+						'/': (_) => const HomeScreen(),
+						'/settings': (_) => const SettingsScreen(),
+					},
 					theme: ThemeData.light(),
 					darkTheme: ThemeData.dark(),
 					themeMode: theme.mode,
-					home: const HomeScreen(),
 				)
+		);
+}
+
+class ThemeButton extends StatelessWidget {
+  const ThemeButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+		Consumer<ThemeModel>(
+			builder: (_, theme, __) => FloatingActionButton(
+				onPressed: theme.toggle,
+				child: Icon(theme.icon),
+			),
 		);
 }
 
@@ -41,11 +79,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
     Scaffold(
-			floatingActionButton: Consumer<ThemeModel>(
-				builder: (_, theme, __) => FloatingActionButton(
-					onPressed: theme.toggle,
-					child: Icon(theme.icon),
-				),
-			),
+			floatingActionButton: const ThemeButton(),
+			body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/settings'),
+          child: const Text('Settings'),
+        ),
+      ),
     );
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+    Scaffold(
+			floatingActionButton: const ThemeButton(),
+			body: Consumer<VolumeModel>(
+				builder: (_, volume, __) => Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+				  crossAxisAlignment: CrossAxisAlignment.center,
+					children: [
+						Text(volume.value.toStringAsFixed(2)),
+						Slider(
+							value: volume.value,
+							max: volume.max,
+							min: volume.min,
+							onChanged: (newValue) => volume.value = newValue
+						),
+					]
+				)
+			)
+		);
 }
